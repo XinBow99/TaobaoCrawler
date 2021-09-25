658fe3e5e22cb4f23e04663e1ce6cc32c6b63120
Jason <asdewq45445@gmail.com>
2021-09-25 19:39:59 +0800

    測試MITMPROXY-1

--- ApiCrawler/mitmproxyPlugin/script.py
+++ ApiCrawler/mitmproxyPlugin/script.py
@@ -5,9 +5,11 @@ def request(flow: http.HTTPFlow) -> None:
     # 將請求新增了一個查詢參數
     if 'tmall' in flow.request.url:
         print(flow.request.headers['cookie'])
+    else:
+        return False
 
 
 def response(flow: http.HTTPFlow) -> None:
     # 將響應頭中新增了一個自定義頭字段
-    flow.response.headers["newheader"] = "foo"
+    flow.response.headers["mit"] = "Testing"
     print(flow.response.text)
